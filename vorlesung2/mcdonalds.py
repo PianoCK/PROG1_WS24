a69be64a4f082160472b291ca97b647a8b9589ff
Christian Krauss <christian.krauss@fh-kiel.de>
2024-10-10 09:55:20 +0200

    vorlesung 2

---
+++ vorlesung2/mcdonalds.py
@@ -0,0 +1,25 @@
+# Initale Variablen
+steuersatz = ""
+# Eingabe
+
+try:
+    preis = float(input("Preis: "))
+except:
+    print ("Das war keine Zahl")
+    exit()
+
+umsatz_reduziert = input("Nehmen Sie das Essen mit nach Hause? (J/N): ")
+
+# Verarbeitung
+if umsatz_reduziert == "J" or umsatz_reduziert == "j":
+    netto = preis / 1.07
+    steuersatz = "7%"
+elif umsatz_reduziert == "N" or umsatz_reduziert == "n":
+    netto = preis / 1.19
+    steuersatz = "19%"
+else:
+   print("Fehler bei der Eingabe")
+
+# Ausgabe
+if steuersatz != "":
+    print(f"Netto {netto:.2f} Euro bei einem Steuersatz von {steuersatz}")
---
+++ vorlesung2/umsatz.py
@@ -0,0 +1,8 @@
+
+umsatz = float(input("Umsatz: "))
+kosten = float(input("Kosten: "))
+
+gewinn = umsatz - kosten
+
+print(f"Der Gewinn beträgt {gewinn:.2f} Euro")
+
